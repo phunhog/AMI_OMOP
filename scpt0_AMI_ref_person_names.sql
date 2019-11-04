bdb3186de3cbb91ae938085080c9910376eb4807
John Higgins <john.h.higgins@Dartmouth.edu>
2019-11-04 13:02:28 -0500

    spdate for table obj scripting
    Signed-off-by: John Higgins <john.h.higgins@Dartmouth.edu>

--- scpt0_AMI_ref_person_names.sql
+++ scpt0_AMI_ref_person_names.sql
@@ -10,6 +10,7 @@
 --readmissions outside of the home facility.
 
 --completed Dh code JH Higgins 11/1/2019
+-- table object scripting added 11/4/2019
 -----------------------------------------------------------------------------------------
 
 /*
@@ -36,6 +37,16 @@ from
 
 
 -- Begin DH code
+Use OMOP_CDM
+
+
+GO
+
+IF OBJECT_ID('dbo.ref_person_names', 'U') IS NOT NULL 
+	DROP table dbo.ref_person_names
+
+GO
+
 SELECT        
 OPer.person_id
 , OPer.person_source_value AS MRN -- this an OMOP CDM integer
@@ -52,11 +63,20 @@ FROM            person AS OPer
 LEFT OUTER JOIN  [DH5772\CLARITY].oClarity.dbo.tblAMIIndexEvent AS N 
 ON [dbo].[udfOMOP_MRN](OPer.person_source_value) = N.mrn
 ORDER BY OPer.person_id
+
+go
 ----------------------------------------------------------
 		
 	
 --Patient Social Security Number
 -- more DH code		
+
+GO
+
+IF OBJECT_ID('dbo.ref_person_SSN', 'U') IS NOT NULL 
+	DROP table dbo.ref_person_SSN
+
+GO
 select
 	OPer.Person_Source_Value	as MRN	--Medical Record Number
 	, S.SSN						as SSN  --Social Security Number
@@ -67,5 +87,7 @@ from
 	left join
 	[DH5772\CLARITY].oClarity.dbo.tblAMIIndexEvent as S
 		on [dbo].[udfOMOP_MRN](OPer.person_source_value) = s.mrn
+
+GO
 ;
 	
