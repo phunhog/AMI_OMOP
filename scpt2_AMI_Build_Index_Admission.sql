0d802fea42dc3342a27e2e7f8132203b413b68fd
John Higgins <john.h.higgins@Dartmouth.edu>
2019-11-18 12:08:54 -0500

    Edited erran tcode
    Signed-off-by: John Higgins <john.h.higgins@Dartmouth.edu>

--- scpt2_AMI_Build_Index_Admission.sql
+++ scpt2_AMI_Build_Index_Admission.sql
@@ -7,30 +7,15 @@ the first AMI encounter, where the primary diagnosis is AMI within the specified
 -----------------------------------------------------------------------------------------
 
 USE OMOP_CDM
-=======
-
-Use OMOP_CDM
-
-
-GO
-
-IF OBJECT_ID('dbo.COHORT_BASE', 'U') IS NOT NULL 
-	DROP table dbo.COHORT_BASE
-
-
 GO
 
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
-
 GO
 
 if exists (select * from sys.objects where name = 'COHORT_BASE' and type = 'u')
     drop table COHORT_BASE
-=======
-
-
 GO
 --drop table AMI.COHORT_BASE if exists;	--Netezza SQL
 
@@ -39,25 +24,20 @@ select
 	  OVO.VISIT_OCCURRENCE_ID 	 	as VISIT_OCCURRENCE_ID
 	, OVO.VISIT_START_DATE 		 	as ADMIT_DATE
 
-	, OVO.VISIT_START_DATE		 	as ADMIT_DATETIME
-	, OVO.VISIT_END_DATE 		 	as DISCHARGE_DATE
-	, OVO.VISIT_END_DATE	 		as DISCHARGE_DATETIME
+	--, OVO.VISIT_START_DATETIME 	 	as ADMIT_DATETIME -- leave the old code intact
+	,Convert(date,Convert(nvarchar(50),OVO.VISIT_START_DATE,113)) as ADMIT_DATETIME
 
-	, OVO.VISIT_START_DATE  	 	as ADMIT_DATETIME
 	, OVO.VISIT_END_DATE 		 	as DISCHARGE_DATE
-	, OVO.VISIT_END_DATE	 	 	as DISCHARGE_DATETIME
+
+	--, OVO.VISIT_END_DATETIME 	 	as DISCHARGE_DATETIME
+	,Convert(date,Convert(nvarchar(50),OVO.VISIT_END_DATE,113)) as DISCHARGE_DATETIME
 
 	, OVO.VISIT_START_DATE 		 	as INDEX_ADMIT_DATE
 	, OVO.VISIT_END_DATE 		 	as INDEX_DISCHARGE_DATE
 	, OCO.CONDITION_SOURCE_VALUE 	as PRIM_DIAG
 	, OVO.PERSON_ID 				as PERSON_ID
 	, OPer.PERSON_SOURCE_VALUE 		as MRN
-
-	, OPer.BIRTH_DATE 				as DOB
-
-	--, OPer.BIRTH_DATE	 			as DOB
-
-
+	, OPer.BIRTH_DATE	 			as DOB
 	, RefPer.FIRST_NAME  			as FIRST_NAME
 	, RefPer.LAST_NAME   			as LAST_NAME
 	, RefPer.MIDDLE_NAME 			as MIDDLE_NAME
@@ -65,12 +45,11 @@ select
 	, OCon2.CONCEPT_NAME 			as RACE
 	, OCon3.CONCEPT_NAME 			as ETHNICITY
 	, OLoc.ZIP 						as ZIPCODE
-
-	, datediff(YEAR,OPer.BIRTH_DATE, OVO.VISIT_START_DATE) as Age_at_Admit
+	, datediff(yyyy,OPer.BIRTH_DATE, OVO.VISIT_START_DATE) as Age_at_Admit
 
 into
 	#COHORT_BASE_part1	--temporary table
-	--temp AMI.COHORT_BASE_part1	--Netezza SQL
+	
 from 
 	VISIT_OCCURRENCE as OVO
 	left join 
@@ -98,45 +77,6 @@ from
 	REF_PERSON_NAMES AS RefPer
 		ON OPer.PERSON_SOURCE_VALUE = RefPer.MRN
 where 
-
-	--, datediff(YYYY,OPer.BIRTH_DATETIME, OVO.VISIT_START_DATE) as Age_at_Admit
-	--, date_part('year', age(OVO.VISIT_START_DATE, OPer.BIRTH_DATETIME)) as Age_at_Admit	--Netezza SQL
---chad starts using temp tables
-into
-	#COHORT_BASE_part1	--temporary table
-	
-from 
-	OMOP_CDM.dbo.visit_occurrence as OVO
-	left join 	OMOP_CDM.dbo.CONDITION_OCCURRENCE as OCO
-		on OVO.VISIT_OCCURRENCE_ID = OCO.VISIT_OCCURRENCE_ID
-	left join OMOP_CDM.dbo.PERSON as OPer
-		on OVO.PERSON_ID = OPer.PERSON_ID
-
-	left join
-	ref_diag_codes as RefDiag
-		on OCO.Condition_Concept_ID = RefDiag.Target_Concept_ID
-
---LOCATION is a reserved word in TSQL so i have to use the fully qualifies object name
-	left join
-	OMOP_CDM.dbo.[LOCATION] as OLoc
-		on OPer.LOCATION_ID = OLoc.LOCATION_ID
-	left join
-	OMOP_CDM.dbo.CONCEPT as OCON1
-		on OCon1.CONCEPT_ID = OPer.GENDER_CONCEPT_ID 
-	left join
-	OMOP_CDM.dbo.CONCEPT as OCON2
-		on OCon2.CONCEPT_ID = OPer.RACE_CONCEPT_ID 
-	left join
-	OMOP_CDM.dbo.CONCEPT as OCON3
-		on OCon3.CONCEPT_ID = OPer.ETHNICITY_CONCEPT_ID 
-	left join
-	OMOP_CDM.dbo.REF_PERSON_NAMES AS RefPer
-		ON OPer.PERSON_SOURCE_VALUE = RefPer.MRN
-
-		------------------
-
-		where 
-
 	OVO.VISIT_CONCEPT_ID = 9201 --Inpatient
 	and 
 		(
@@ -151,23 +91,11 @@ from
 	--is being used to identify principal diagnosis. This may need to be reworked in the
 	--Vanderbilt implementation of OMOP to exclude the CONDITION_STATUS_CONCEPT_ID because
 	--the current implementation seems to be nonstandard.
-
-		*/
+	*/
 	and cast(convert(char(11), OVO.Visit_end_date, 113) as datetime) >= '2007-011-01 00:00:00.000'
 	and cast(convert(char(11), OVO.VISIT_END_DATE, 113) as datetime)  <= '2017-011-01 00:00:00.000'
 	and RefDiag.DIAGNOSIS = 'AMI'
-
-
 ;
-
-
-	*/
-	--and OVO.VISIT_END_DATE >= '20070101'
-	--and OVO.VISIT_END_DATE < '20170101'
-	--and RefDiag.DIAGNOSIS = 'AMI'
---;Build_Index
---------------------------------------------------------
-
 
 select
 	*
@@ -180,17 +108,8 @@ from
 			,ROW_NUMBER() OVER (PARTITION BY PERSON_ID ORDER BY ADMIT_DATE ASC, Prim_Diag ASC) AS Date_Row
 		from
 			#COHORT_BASE_part1
-
 			--AMI.COHORT_BASE_part1	--Netezza SQL
 	) sub
 where Date_Row = 1
-
--- cleanup
-drop table #COHORT_BASE_part1
 ;
---End of PART 1--------------------------------------------------------------------------
-
-			
-	) sub
-where Date_Row = 1
-
+--End of PART 1--------------------
