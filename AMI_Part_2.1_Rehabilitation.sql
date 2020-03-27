5e9768ef2cabdfb60e549364ebb986f34314e39d
John Higgins <john.h.higgins@Dartmouth.edu>
2020-03-27 12:17:57 -0400

    edited to match local obj names
    Signed-off-by: John Higgins <john.h.higgins@Dartmouth.edu>

--- AMI_Part_2.1_Rehabilitation.sql
+++ AMI_Part_2.1_Rehabilitation.sql
@@ -9,13 +9,21 @@ PART 2.1: Readmissions for rehabilitation to be excluded.
 
 Rehab flag - below 
 
+
+Update 3/37/2020
+Edited & updated to match local Dartmouth object names
+
 */
 
 --Rehabilitation Flag--------------------------------------------------------------------------
 
 
-drop table if exists AMI.Table1_Rehabilitation;
+USE OMOP_CDM
 
+go
+
+IF OBJECT_ID('dbo.Table1_Rehabilitation', 'U') IS NOT NULL 
+  DROP TABLE Table1_Rehabilitation
 select 
 	 ACB2.PERSON_ID
 	,ACB2.VISIT_OCCURRENCE_ID
@@ -28,15 +36,15 @@ select
 				else 0 
 			end
 		) as Rehab_Flag
-into AMI.Table1_Rehabilitation
+into Table1_Rehabilitation
 from 
-	AMI.COHORT_BASE_2 as ACB2
+	COHORT_BASE_2 as ACB2
 	left join 
-	OMOP.CONDITION_OCCURRENCE as OCO
+	CONDITION_OCCURRENCE as OCO
 		on ACB2.PERSON_ID = OCO.PERSON_ID
 		and ACB2.VISIT_OCCURRENCE_ID = OCO.VISIT_OCCURRENCE_ID
 	left join 
-	AMI.Ref_Conditions_SNOMED as ARef
+	Ref_Conditions_SNOMED as ARef
 		on OCO.CONDITION_CONCEPT_ID = ARef.TARGET_CONCEPT_ID
 group by 
 	 ACB2.PERSON_ID
