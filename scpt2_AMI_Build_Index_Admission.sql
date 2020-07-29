42fef38051a7e3d49c6b40de9179eca128f63f8f
John Higgins <john.h.higgins@Dartmouth.edu>
2020-07-29 09:44:33 -0400

    update comments
    Signed-off-by: John Higgins <john.h.higgins@Dartmouth.edu>

--- scpt2_AMI_Build_Index_Admission.sql
+++ scpt2_AMI_Build_Index_Admission.sql
@@ -72,8 +72,9 @@ from
 	left join 
 	PERSON as OPer
 		on OVO.PERSON_ID = OPer.PERSON_ID
+------------------------------------------------------------
 	/*
-	with chad 7/24
+	Edited with chad 7/24 over phone
 	left outer join
 	REF_DIAG_CODES as RefDiag
 		on OCO.Condition_Concept_ID = RefDiag.Target_Concept_ID
@@ -81,7 +82,7 @@ from
 	join
 	[dbo].[ref_diag_codes] as RefDiag
 		on OCO.Condition_source_value = RefDiag.SOURCE_CONCEPT_CODE
-
+---------------------------------------------------------------------------
 	left join
 	[LOCATION] as OLoc
 		on OPer.LOCATION_ID = OLoc.LOCATION_ID
